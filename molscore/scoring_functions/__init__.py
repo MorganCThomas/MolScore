7839900886580f2811349c61efd4af217638d704
MorganCThomas <mct50@cam.ac.uk>
2021-04-30 14:45:30 +0100

    adding chemprop to __init__

--- molscore/scoring_functions/__init__.py
+++ molscore/scoring_functions/__init__.py
@@ -9,6 +9,7 @@ from molscore.scoring_functions.substructure_filters import SubstructureFilters
 from molscore.scoring_functions.substructure_match import SubstructureMatch
 from molscore.scoring_functions.sklearn_model import SKLearnModel, EnsembleSKLearnModel
 from molscore.scoring_functions.rascore_xgb import RAScore_XGB
+from molscore.scoring_functions.chemprop import ChemPropModel
 
 all_scoring_functions = [
     ROCS,
@@ -22,5 +23,6 @@ all_scoring_functions = [
     SubstructureMatch,
     SKLearnModel,
     EnsembleSKLearnModel,
-    RAScore_XGB
+    RAScore_XGB,
+    ChemPropModel
 ]
