c17588ac9961231269c795eb7eb8091fbe019d52
MorganCThomas <mct50@cam.ac.uk>
2021-09-02 21:28:30 +0100

    comment chemprop and activitymodel

--- molscore/scoring_functions/__init__.py
+++ molscore/scoring_functions/__init__.py
@@ -9,7 +9,7 @@ from molscore.scoring_functions.substructure_filters import SubstructureFilters
 from molscore.scoring_functions.substructure_match import SubstructureMatch
 from molscore.scoring_functions.sklearn_model import SKLearnModel, EnsembleSKLearnModel
 from molscore.scoring_functions.rascore_xgb import RAScore_XGB
-from molscore.scoring_functions.chemprop import ChemPropModel
+#from molscore.scoring_functions.chemprop import ChemPropModel
 
 all_scoring_functions = [
     ROCS,
@@ -24,5 +24,5 @@ all_scoring_functions = [
     SKLearnModel,
     EnsembleSKLearnModel,
     RAScore_XGB,
-    ChemPropModel
+#    ChemPropModel
 ]
