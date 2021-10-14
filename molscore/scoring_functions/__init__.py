515d5418228cb809ad5cb53a94944b133375a8b3
MorganCThomas <mct50@cam.ac.uk>
2021-10-14 22:35:12 +0100

    add Isomer to __init__

--- molscore/scoring_functions/__init__.py
+++ molscore/scoring_functions/__init__.py
@@ -5,6 +5,7 @@ from molscore.scoring_functions.oedock import FRED
 from molscore.scoring_functions.tanimoto import TanimotoSimilarity
 #from molscore.scoring_functions.reinvent_svm import ActivityModel
 from molscore.scoring_functions.descriptors import RDKitDescriptors
+from molscore.scoring_functions.isomer import Isomer
 from molscore.scoring_functions.substructure_filters import SubstructureFilters
 from molscore.scoring_functions.substructure_match import SubstructureMatch
 from molscore.scoring_functions.sklearn_model import SKLearnModel, EnsembleSKLearnModel
@@ -12,17 +13,18 @@ from molscore.scoring_functions.rascore_xgb import RAScore_XGB
 #from molscore.scoring_functions.chemprop import ChemPropModel
 
 all_scoring_functions = [
-    ROCS,
-    GlideDock,
-    GlideDockFromROCS,
-    SminaDock,
-    FRED,
-    RDKitDescriptors,
     TanimotoSimilarity,
+    Isomer,
+    RDKitDescriptors,
     SubstructureFilters,
     SubstructureMatch,
     SKLearnModel,
     EnsembleSKLearnModel,
     RAScore_XGB,
+    ROCS,
+    GlideDock,
+    GlideDockFromROCS,
+    SminaDock,
+    FRED
 #    ChemPropModel
 ]
