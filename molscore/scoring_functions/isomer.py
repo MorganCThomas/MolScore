b90f98bb7caaf63720c46c9ad7aa0c0f7b8bf0b1
MorganCThomas <mct50@cam.ac.uk>
2021-10-14 22:32:49 +0100

    add isomer task to scoring functions, quite bespoke score modification

---
+++ molscore/scoring_functions/isomer.py
@@ -0,0 +1,85 @@
+import re
+from rdkit.Chem import Descriptors, QED, Crippen
+from rdkit.Chem import AllChem as Chem
+from molscore.utils.score_modifiers import gauss
+from molscore.utils.score_methods import gmean
+
+
+class Isomer:
+    """
+    Score structures according to molecular formula:
+         re-implementation of GuacaMol https://pubs.acs.org/doi/pdf/10.1021/acs.jcim.8b00839
+    """
+
+    return_metrics = ['isomer_score']
+
+    def __init__(self, prefix: str, molecular_formula: str, **kwargs):
+        """
+        :param prefix: Prefix to identify scoring function instance (e.g., C2H6)
+        :param molecular_formula: Formula that'll be broken down for scoring (e.g., C2H6)
+        """
+        self.prefix = prefix
+        self.ref_formula = molecular_formula
+        self.ref_elements = self.formula2elements(self.ref_formula)
+
+    @staticmethod
+    def formula2elements(formula):
+        """
+        Use regex to retrieve elements and counts
+        """
+        components = re.findall('[a-zA-Z]+[0-9]+', formula)
+        elements = {}
+        for c in components:
+            elements[re.search('[a-zA-Z]+', c).group()] = int(re.search('[0-9]+', c).group())
+        return elements
+
+    def calculate_isomer_score(self, smi):
+        """
+        Calculate isomer score based on geometric mean of Gaussian modifiers applied to element counts.
+        :param smi:
+        :return:
+        """
+        mol = Chem.MolFromSmiles(smi)
+        if mol:
+            query_formula = Descriptors.rdMolDescriptors.CalcMolFormula(mol)
+            query_elements = self.formula2elements(query_formula)
+            elements = set(self.ref_elements)
+            elements.update(query_elements)
+            scores = []
+            # Add per element scores
+            for e in elements:
+                scores.append(gauss(x=query_elements[e] if e in query_elements else 0,
+                                    objective='range',
+                                    mu=self.ref_elements[e] if e in self.ref_elements else 0,
+                                    sigma=1))
+            # Add total atoms score
+            scores.append(gauss(x=sum(query_elements.values()),
+                                objective='range',
+                                mu=sum(self.ref_elements.values()),
+                                sigma=2))
+            score = gmean(scores)
+        else:
+            score = 0.0
+        return score
+
+    def __call__(self, smiles: list, **kwargs):
+        """
+       Calculate Isomer scores for a given list of SMILES.
+       :param smiles: List of SMILES strings
+       :param kwargs: Ignored
+       :return: List of dicts i.e. [{'smiles': smi, 'metric': 'value', ...}, ...]
+       """
+        results = [{'smiles': smi,
+                    f'{self.prefix}_{self.return_metrics[0]}': self.calculate_isomer_score(smi)}
+                   for smi in smiles]
+        return results
+
+
+if __name__ == '__main__':
+    from molscore.test import MockGenerator
+    mg = MockGenerator()
+    iso = Isomer('C12H24', 'C12H24')
+    iso(mg.sample(5))
+
+
+
