fada727f1e57a8d3877a6993d98c7db325bfc685
MorganCThomas <mct50@cam.ac.uk>
2022-04-28 22:48:32 +0100

    add test files

--- molscore/test/__init__.py
+++ molscore/test/__init__.py
@@ -1,1 +1,6 @@
+import os
 from molscore.test.mock_generator import MockGenerator
+
+test_files = {
+    'glide_grid': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'DRD2_6CM4_grid.zip')
+}
