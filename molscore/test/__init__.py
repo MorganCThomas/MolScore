de01aab76acb2de62a7ccc61427cf7c54b23433b
MorganCThomas <mct50@cam.ac.uk>
2022-12-14 14:27:27 +0000

    refactoring tests

--- molscore/test/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from molscore.test.mock_generator import MockGenerator
-
-test_files = {
-    'GlideDock_grid': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'DRD2_6CM4_grid.zip'),
-    'SminaDock_receptor': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'DRD2_6CM4p_rec.pdbqt'),
-    'SminaDock_ref_ligand': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'DRD2_risperidone.sdf')
-}
