7a834c860e1b61359ae9bb63620898f300f79cb9
MorganCThomas <mct50@cam.ac.uk>
2023-02-01 15:20:03 +0000

    update external server

--- molscore/scoring_functions/external_server.py
+++ molscore/scoring_functions/external_server.py
@@ -3,6 +3,7 @@ import requests
 import argparse
 import logging
 import numpy as np
+from io import StringIO
 from functools import partial
 from tempfile import NamedTemporaryFile
 
@@ -38,23 +39,27 @@ class POSTServer:
         self.input_format = input_format
         self.output_value = output_value
 
-    def prepare_input(self, smiles, names, file):
+    def prepare_input(self, smiles, names):
         """ Write input file in specified format. """
+        sio = StringIO()
         if self.input_format == 'smi':
             for smi in smiles:
-                file.write(f'{smi}\n')
+                sio.write(f'{smi}\n')
         elif self.input_format == 'sdf':
-            writer = Chem.SDWriter(file)
+            writer = Chem.SDWriter(sio)
             for smi, name in zip(smiles, names):
                 mol = get_mol(smi)
                 mol.SetProp('_Name', name)
                 writer.write(mol)
+            writer.flush()
             writer.close()
         else:
             raise ValueError(f"Unrecognized input format: {self.input_format}")
-        return 
+        output = sio.getvalue()
+        sio.close()
+        return output
 
-    def score(self, smiles: list, directory: str, file_names: list, **kwargs):
+    def score(self, smiles: list, file_names: list, **kwargs):
         """
         Calculate scores for Tanimoto given a list of SMILES.
         :param smiles: List of SMILES strings
@@ -64,34 +69,32 @@ class POSTServer:
         results = [{'smiles': smi} for smi in smiles]
         # Only submit valid smiles
         valid_smiles = [i for i, smi in enumerate(smiles) if get_mol(smi)]
-        # Use tempfile
-        with NamedTemporaryFile(mode='w+t', suffix=f".{self.input_format}") as tfile:
-            self.prepare_input(
-                smiles=[smiles[i] for i in valid_smiles],
-                names=[file_names[i] for i in valid_smiles],
-                file=tfile
-                )
-            # Submit
-            res = requests.post(self.server_address, data={"sdf": tfile.name})
-            data = res.json()['data']
-            all_keys = set()  # Keep record of keys
-            for i, out in zip(valid_smiles, data):  # Ensure input and output are equal length
-                for k in out:
-                    all_keys.add(k)
-                    value = out[k] if out[k] else 0.0
-                    results[i].update({f'{self.prefix}_{k}': value})
-                    # Record named value twice as this is the name used
-                    if k == self.output_value:
-                        results[i].update({f'{self.prefix}_{self.output_value}': value})
+        # Prepare input
+        input_data = self.prepare_input(
+            smiles=[smiles[i] for i in valid_smiles],
+            names=[file_names[i] for i in valid_smiles]
+            )
+        # Submit
+        res = requests.post(self.server_address, data={"sdf": input_data})
+        output_data = res.json()['data']
+        all_keys = set()  # Keep record of keys
+        for i, out in zip(valid_smiles, output_data):  # Ensure input and output are equal length
+            for k in out:
+                all_keys.add(k)
+                value = out[k] if out[k] else 0.0
+                results[i].update({f'{self.prefix}_{k}': value})
+                # Record named value twice as this is the name used
+                if k == self.output_value:
+                    results[i].update({f'{self.prefix}_{self.return_metrics[0]}': value})
 
         # Add keys as 0.0 for invalid molecules
         for i, res in enumerate(results):
             if i not in valid_smiles:
                 res.update({f'{self.prefix}_{k}': 0.0 for k in all_keys})
         
-        return res
+        return results
     
-    def __call__(self, smiles: list, directory: str, file_names: list, **kwargs):
+    def __call__(self, smiles: list, file_names: list, **kwargs):
         """
         Calculate scores for Tanimoto given a list of SMILES.
         :param smiles: List of SMILES strings
