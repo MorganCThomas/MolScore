2575b0c22e3bc3f61e93612c15b45f088d7e2e70
MorganCThomas <mct50@cam.ac.uk>
2023-02-01 18:19:25 +0000

    clean up

--- molscore/scoring_functions/__init__.py
+++ molscore/scoring_functions/__init__.py
@@ -1,14 +1,11 @@
-from molscore.scoring_functions.glide import GlideDock, GlideDockFromROCS
+from molscore.scoring_functions.glide import GlideDock
 from molscore.scoring_functions.plants import PLANTSDock
 from molscore.scoring_functions.gold import GOLDDock, ChemPLPGOLDDock, ASPGOLDDock, ChemScoreGOLDDock, GoldScoreGOLDDock
 from molscore.scoring_functions.smina import SminaDock
-from molscore.scoring_functions.rocs import ROCS
-from molscore.scoring_functions.oedock import OEDock
 from molscore.scoring_functions.similarity import MolecularSimilarity, TanimotoSimilarity
 from molscore.scoring_functions.applicability_domain import ApplicabilityDomain
 from molscore.scoring_functions.external_server import POSTServer
 from molscore.scoring_functions.align3d import Align3D
-#from molscore.scoring_functions.reinvent_svm import ActivityModel
 from molscore.scoring_functions.pidgin import PIDGIN
 from molscore.scoring_functions.descriptors import MolecularDescriptors, RDKitDescriptors
 from molscore.scoring_functions.isomer import Isomer
@@ -16,7 +13,6 @@ from molscore.scoring_functions.substructure_filters import SubstructureFilters
 from molscore.scoring_functions.substructure_match import SubstructureMatch
 from molscore.scoring_functions.sklearn_model import SKLearnModel, EnsembleSKLearnModel
 from molscore.scoring_functions.rascore_xgb import RAScore_XGB
-#from molscore.scoring_functions.chemprop import ChemPropModel
 
 all_scoring_functions = [
     MolecularSimilarity, 
@@ -32,17 +28,30 @@ all_scoring_functions = [
     POSTServer,
     PIDGIN,
     RAScore_XGB,
-    ROCS,
     Align3D,
     GlideDock,
-    GlideDockFromROCS,
     SminaDock,
     PLANTSDock,
     GOLDDock,
     ChemPLPGOLDDock,
     ASPGOLDDock,
     ChemScoreGOLDDock,
-    GoldScoreGOLDDock,
-    OEDock
-#    ChemPropModel
+    GoldScoreGOLDDock
 ]
+
+try:
+    from molscore.scoring_functions.rocs import ROCS, GlideDockFromROCS
+    from molscore.scoring_functions.oedock import OEDock
+    all_scoring_functions += [ROCS, GlideDockFromROCS, OEDock]
+except ImportError as e:
+    print(f"{e}\nTo use openeye scoring functions please install openeye and acquire a license")
+
+try:
+    from molscore.scoring_functions.chemprop import ChemPropModel
+    all_scoring_functions += [ChemPropModel]
+except ImportError as e:
+    print(f"{e}\nTo use ChemProp please install the relevant version of PyTorch")
+
+# ----- To load the classical DRD2 test case requires an old version of Scikit-learn
+#from molscore.scoring_functions.reinvent_svm import ActivityModel
+#all_scoring_functions += [ActivityModel]
