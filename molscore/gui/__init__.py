7f8564d58ad61ae25c93f0ed88d7d1db7c2a8c10
MorganCThomas <mct50@cam.ac.uk>
2023-02-23 19:58:52 +0000

    update path

--- molscore/gui/__init__.py
+++ molscore/gui/__init__.py
@@ -1,3 +1,3 @@
 import os
 
-monitor_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'st_monitor.py')
+monitor_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'monitor.py')
