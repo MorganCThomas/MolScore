44af459074d5cbe829fee71f6ffa0eaa3f6b1e8a
MorganCThomas <morganthomas263@gmail.com>
2024-04-06 15:01:18 +0200

    typo

--- molscore/__init__.py
+++ molscore/__init__.py
@@ -3,9 +3,9 @@ __version__ = "1.4"
 # Import lib resources
 import sys
 if sys.version_info[1] < 9:
-    from importlib import resources
-else:
     import importlib_resources as resources 
+else:
+    from importlib import resources
 
 # Import managers 
 from molscore.manager import MolScore, MolScoreBenchmark
