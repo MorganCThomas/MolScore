33667f9696632cea994ca86fce3b8c9456a5860f
MorganCThomas <morganthomas263@gmail.com>
2025-05-08 11:18:55 +0200

    update version

--- molscore/__init__.py
+++ molscore/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.9.2"
+__version__ = "1.9.3"
 __all__ = ["MolScore", "MolScoreBenchmark", "MolScoreCurriculum", "MockGenerator"]
 
 # Import lib resources depending on python version
