e32f24560f57642f852b96218d55da38ecdb232d
MorganCThomas <morganthomas263@gmail.com>
2025-05-22 13:33:08 +0200

    add oracle budget option

--- molscore/tests/conftest.py
+++ molscore/tests/conftest.py
@@ -1,3 +1,25 @@
+from pathlib import Path
+import pytest
+from molscore import MockGenerator
+    
+@pytest.fixture
+def test_out_dir():
+    return Path(__file__).parent / "test_out"
+
+@pytest.fixture
+def ref_smiles():
+    mg = MockGenerator(seed_no=123)
+    return mg.sample(5)
+
+@pytest.fixture
+def in_smiles():
+    mg = MockGenerator(seed_no=321, augment_invalids=True, augment_duplicates=False)
+    return mg.sample(10)
+
+@pytest.fixture
+def smiles_generator():
+    return MockGenerator(seed_no=231, augment_invalids=True, augment_duplicates=True)
+
 def pytest_addoption(parser):
     parser.addoption(
         "--configs",
@@ -5,3 +27,9 @@ def pytest_addoption(parser):
         nargs="+",
         help="One or more configuration definitions, paths, or directories.",
     )
+    parser.addoption(
+        "--full", action="store_true", default=False, help="Run full parameter grid"
+    )
+    
+def pytest_configure(config):
+    config.addinivalue_line("markers", "full: mark test as part of full parameter grid")
