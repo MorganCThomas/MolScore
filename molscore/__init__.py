f090e24e711b9a67afcb30fdf1f5bf8016173b52
MorganCThomas <morganthomas263@gmail.com>
2025-09-23 09:42:18 +0200

    remove upper python requirement

--- molscore/__init__.py
+++ molscore/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.9.4"
+__version__ = "1.9.5"
 __all__ = ["MolScore", "MolScoreBenchmark", "MolScoreCurriculum", "MockGenerator"]
 
 # Import lib resources depending on python version
